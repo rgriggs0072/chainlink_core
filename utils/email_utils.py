1899663499616db48554ac162b36f09830355aaf
Randy G <rjgriggs0072@gmail.com>
2025-12-01 16:53:15 -0800

    fixed email link

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -11,9 +11,45 @@ def get_mailjet_server():
     server.login(creds["API_KEY"], creds["SECRET_KEY"])
     return server
 
-def send_reset_email(email, token, first_name="User"):
+
+def send_reset_email(email: str, token: str, first_name: str = "User") -> None:
+    """
+    Send a password reset email with a time-limited token.
+
+    - Uses the deployed Streamlit base URL from st.secrets["app"]["base_url"].
+    - Builds a reset link that hits the /reset_password route with ?token=...
+    """
     sender_email = "randy@chainlinkanalytics.com"
-    reset_link = f"http://localhost:8501/reset_password?token={token}"
+
+    # Get base URL of the deployed app, e.g. "https://chainlinkcore-main.streamlit.app"
+    app_base_url = st.secrets["app"]["base_url"].rstrip("/")
+
+    # If your reset page is a dedicated /reset_password route (Streamlit page file),
+    # this is correct:
+    reset_link = f"{app_base_url}/reset_password?token={token}"
+
+    # If instead your router expects something like /?page=reset_password&token=...
+    # then use this line instead:
+    # reset_link = f"{app_base_url}/?page=reset_password&token={token}"
+
+    subject = "Chainlink Core – Password Reset"
+    body = f"""
+Hi {first_name},
+
+We received a request to reset your Chainlink Core password.
+
+Click the link below to set a new password (this link may expire after a short time):
+
+{reset_link}
+
+If you did not request this, you can safely ignore this email.
+
+– Chainlink Analytics
+"""
+
+    # TODO: plug this back into your Mailjet / SMTP sender
+    # send_email_via_mailjet(sender_email, email, subject, body)
+
 
     subject = "Chainlink Analytics Password Reset"
     html = email_templates.reset_password_template(first_name, reset_link)
